365f72b54ec4d663dbf6eae8796b85fd581722ea
Bay Eggex <79448667+bayeggex@users.noreply.github.com>
2026-06-19 22:54:14 +0300

    Add files via upload

---
+++ src/ab_logo.c
@@ -0,0 +1,16 @@
+/**
+ * @file ab_logo.c
+ * \brief
+ * The ARDUBOY logo bitmap.
+ * Writer @BayEggex
+ * originally from Chris J. Martinez, Kevin Bates, Josh Goebel, Scott Allen, Ross O. Shoger
+ */
+
+#include <avr/pgmspace.h>
+
+#ifndef ARDUBOY_LOGO_CREATED
+#define ARDUBOY_LOGO_CREATED
+//Saved arround 505-520 bytes of flash
+//if you want to add them back
+//check the readme file
+#endif
